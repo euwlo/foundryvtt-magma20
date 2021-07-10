f8d14a6a0c3c46f13f9bcf932abdf00234acf69f
Wesley Oliveira <euwlo31@gmail.com>
2021-07-09 21:34:16 -0300

    TPK dice added

---
+++ magma20/scripts/TPK.js
@@ -0,0 +1,51 @@
+Hooks.once('diceSoNiceReady', (dice3d) => {
+	dice3d.addSystem({id: "tpk20", name: "TPK"}, false);
+	dice3d.addDicePreset({
+		type: "d2",
+		labels: "",
+		modelFile: "modules/magma20/models/TPK/tpk_d2.gltf",
+		system: "tpk20"
+	});
+		dice3d.addDicePreset({
+		type: "d4",
+		labels: "",
+		modelFile: "modules/magma20/models/TPK/tpk_d4.gltf",
+		system: "tpk20"
+	});
+		dice3d.addDicePreset({
+		type: "d6",
+		labels: "",
+		modelFile: "modules/magma20/models/TPK/tpk_d6.gltf",
+		system: "tpk20"
+	});
+		dice3d.addDicePreset({
+		type: "d8",
+		labels: "",
+		modelFile: "modules/magma20/models/TPK/tpk_d8.gltf",
+		system: "tpk20"
+	});
+		dice3d.addDicePreset({
+		type: "d10",
+		labels: "",
+		modelFile: "modules/magma20/models/TPK/tpk_d10.gltf",
+		system: "tpk20"
+	});
+		dice3d.addDicePreset({
+		type: "d100",
+		labels: "",
+		modelFile: "modules/magma20/models/TPK/tpk_d100.gltf",
+		system: "tpk20"
+	});
+		dice3d.addDicePreset({
+		type: "d12",
+		labels: "",
+		modelFile: "modules/magma20/models/TPK/tpk_d12.gltf",
+		system: "tpk20"
+	});	
+		dice3d.addDicePreset({
+		type: "d20",
+		labels: "",
+		modelFile: "modules/magma20/models/TPK/tpk_d20.gltf",
+		system: "tpk20"
+	});
+});
--- magma20/scripts/magma20.js
+++ magma20/scripts/magma20.js
@@ -3,49 +3,49 @@ Hooks.once('diceSoNiceReady', (dice3d) => {
 	dice3d.addDicePreset({
 		type: "d2",
 		labels: "",
-		modelFile: "modules/magma20/models/magma20_d2.gltf",
+		modelFile: "modules/magma20/models/magmaDice/magma20_d2.gltf",
 		system: "magma20"
 	});
 		dice3d.addDicePreset({
 		type: "d4",
 		labels: "",
-		modelFile: "modules/magma20/models/magma20_d4.gltf",
+		modelFile: "modules/magma20/models/magmaDice/magma20_d4.gltf",
 		system: "magma20"
 	});
 		dice3d.addDicePreset({
 		type: "d6",
 		labels: "",
-		modelFile: "modules/magma20/models/magma20_d6.gltf",
+		modelFile: "modules/magma20/models/magmaDice/magma20_d6.gltf",
 		system: "magma20"
 	});
 		dice3d.addDicePreset({
 		type: "d8",
 		labels: "",
-		modelFile: "modules/magma20/models/magma20_d8.gltf",
+		modelFile: "modules/magma20/models/magmaDice/magma20_d8.gltf",
 		system: "magma20"
 	});
 		dice3d.addDicePreset({
 		type: "d10",
 		labels: "",
-		modelFile: "modules/magma20/models/magma20_d10.gltf",
+		modelFile: "modules/magma20/models/magmaDice/magma20_d10.gltf",
 		system: "magma20"
 	});
 		dice3d.addDicePreset({
 		type: "d100",
 		labels: "",
-		modelFile: "modules/magma20/models/magma20_d100.gltf",
+		modelFile: "modules/magma20/models/magmaDice/magma20_d100.gltf",
 		system: "magma20"
 	});
 		dice3d.addDicePreset({
 		type: "d12",
 		labels: "",
-		modelFile: "modules/magma20/models/magma20_d12.gltf",
+		modelFile: "modules/magma20/models/magmaDice/magma20_d12.gltf",
 		system: "magma20"
 	});	
 		dice3d.addDicePreset({
 		type: "d20",
 		labels: "",
-		modelFile: "modules/magma20/models/magma20_d20.gltf",
+		modelFile: "modules/magma20/models/magmaDice/magma20_d20.gltf",
 		system: "magma20"
 	});
 });
